951d4f579a0c3ef56e6f00bdbcfd922a40109285
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-10-27 13:55:33 +0700

    Initial commit

---
+++ lib/App/GetoptLongDescriptiveUtils.pm
@@ -0,0 +1,21 @@
+package App::GetoptLongDescriptiveUtils;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Collection of CLI utilities related to Getopt::Long::Descriptive
+
+=head1 DESCRIPTION
+
+This distribution provides the following command-line utilities related to
+L<Getopt::Long::Descriptive>:
+
+#INSERT_EXECS_LIST
+
+
+=head1 SEE ALSO
+
+L<Getopt::Long::Descriptive>
+
+=cut
